bab7598066b1a9b913459016a94aa79b3e412a18
Naoji <84818516+n4oji@users.noreply.github.com>
2022-11-06 21:46:45 -0300

    create event

--- my_website/events/forms.py
+++ my_website/events/forms.py
@@ -1,6 +1,6 @@
 from django import forms 
 from django.forms import ModelForm
-from .models import Venue 
+from .models import Venue,Event
 
 class VenueForm(ModelForm):
 	class Meta:
@@ -22,4 +22,26 @@ class VenueForm(ModelForm):
 			'phone': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Phone'}),
 			'website': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Website'}),
 			'email_address': forms.EmailInput(attrs={'class':'form-control', 'placeholder':'Email'}),
+		}
+
+class EventForm(ModelForm):
+	class Meta:
+		model = Event
+		# fields = "__all__"
+		fields = ('name', 'event_date', 'venue', 'manager', 'attendees', 'description')
+		labels = {
+			'name': '',
+			'event_date': 'YYYY-MM-DD HH:MM:SS',
+			'venue': 'Venue',
+			'manager': 'Manager',
+			'attendees': 'Attendees',
+			'description': '',
+		}
+		widgets = {
+			'name': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Event Name'}),
+			'event_date': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Date'}),
+			'venue': forms.Select(attrs={'class':'form-select','placeholder':'Venue'}),
+			'manager': forms.Select(attrs={'class':'form-select', 'placeholder':'Manager'}),
+			'attendees': forms.SelectMultiple(attrs={'class':'form-control', 'placeholder':'Attendees'}),
+			'description': forms.Textarea(attrs={'class':'form-control', 'placeholder':'Description'}),
 		}
--- my_website/events/views.py
+++ my_website/events/views.py
@@ -4,7 +4,7 @@ import calendar
 from calendar import HTMLCalendar
 from datetime import datetime
 from .models import Event, Venue
-from .forms import VenueForm
+from .forms import VenueForm, EventForm
 
 
 def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
@@ -80,3 +80,18 @@ def update_venue(request, venue_id):
 		return redirect('list-venues')
 
 	return render(request, 'events/update_venue.html', {'venue': venue, 'form': form})
+
+def add_event(request):
+	submitted = False
+	if request.method == "POST":
+		form = EventForm(request.POST)
+		if form.is_valid():
+			form.save()
+
+			return HttpResponseRedirect('/add_event?submitted=True')
+	else:
+		form = EventForm
+		if 'submitted' in request.GET:
+			submitted = True
+
+	return render(request, 'events/add_event.html', {'form': form, 'submitted': submitted})
