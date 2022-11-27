1fc049b2e7b9662051cf02b5fe7063511abb26c2
Naoji <84818516+n4oji@users.noreply.github.com>
2022-11-26 22:40:35 -0300

    user login system

--- my_website/my_website/urls.py
+++ my_website/my_website/urls.py
@@ -6,7 +6,15 @@ from events import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('events.urls'))
+    path('', include('events.urls')),
+    path('members/', include('django.contrib.auth.urls')),
+    path('members/', include('members.urls')),
+    
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+#configure admin
+admin.site.site_header = "My Club Administration Page"
+admin.site.site_title = "Club Page Admin"
+admin.site.index_title = "Admin Area"
